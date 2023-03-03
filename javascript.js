375635aa743b1b777346bbd9f7a768223ac8c251
Noah <118230017+jencz@users.noreply.github.com>
2023-03-02 20:53:49 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function validate()
+{
+    let password = document.getElementById('password').value;
+    if (password == "12345")
+    {
+        window.open("./button-clicker.html");
+        return false;
+    }
+    else
+    {
+        alert("Invalid Password. Please try again.");
+    }
+}
+
+function GetHelp() 
+{
+    let x = document.getElementById('Help');
+    if (x.style.display == 'none') 
+    {
+        x.style.display = 'block';
+    } 
+    else 
+    {
+        x.style.display = 'none';
+    }
+}
